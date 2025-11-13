e2db9e4b5c3fc75b89b021fcb365357ea3bec33a
Abdulqader07 <prejuse.ix@gmail.com>
2025-11-13 15:02:51 +0300

    adding a test that checks if the user already exists

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,13 +1,18 @@
 const app = require('../server');
 const request = require('supertest');
+const DataBase = require('better-sqlite3');
+
+// Connecting the dataBase
+const db = DataBase('database2.db');
 
 // Testing units file
 
-describe("authorization and authentication unit tests", ()=>{
-    const time = new Date();
+describe("sign up tests", ()=>{
 
     // test if the user data is valid in signup route
-    it("signup tokens are created", async () =>{
+    it("create a user if they don't exist", async () =>{
+        const time = new Date();
+
         const data = {
             "username": `test${time.getMilliseconds()}`,
             "email": `test${time.getMilliseconds()}@test.com`,
@@ -17,6 +22,26 @@ describe("authorization and authentication unit tests", ()=>{
         const response = await request(app).post('/signup').send(data);
 
         expect(response.statusCode).toBe(201);
-    })
+    });
+
+    it("reject adding a user if they exists", async () => {
+        const data = {
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'password',
+            'passwordConfirmation': 'password'
+        }
+        const addUser = await db.prepare(`
+            INSERT INTO users (username, email, password) VALUES (?, ?, ?)`);
+        addUser.run(data['username'], data['email'], data['password']);
+
+        const response = await request(app).post('/signup').send(data);
+
+        expect(response.statusCode).toBe(409);
+
+        const deleteUser = db.prepare(`
+            DELETE FROM users WHERE email LIKE ?`);
+        deleteUser.run(data['email']);
+    });
 
 });
